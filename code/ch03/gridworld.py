ccb46d4603b64cec75f65493defc1cf7ee855d59
Sergey Litvinov <slitvinov@gmail.com>
2020-01-20 15:04:58 +0100

    add gridworld

---
+++ code/ch03/gridworld.py
@@ -0,0 +1,54 @@
+#######################################################################
+# Copyright (C)                                                       #
+# 2016-2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)             #
+# 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+import sys
+import numpy as np
+
+WORLD_SIZE = 5
+A_POS = [0, 1]
+A_PRIME_POS = [4, 1]
+B_POS = [0, 3]
+B_PRIME_POS = [2, 3]
+DISCOUNT = 0.9
+EPS = 1e-8
+
+ACTIONS = ((0, -1), (-1, 0), (0, 1), (1, 0))
+ACTION_PROB = 0.25
+def step(state, action):
+    if state == A_POS:
+        return A_PRIME_POS, 10
+    if state == B_POS:
+        return B_PRIME_POS, 5
+    next_state = ( state[0] + action[0], state[1] + action[1] )
+    x, y = next_state
+    if x < 0 or x >= WORLD_SIZE or y < 0 or y >= WORLD_SIZE:
+        reward = -1.0
+        next_state = state
+    else:
+        reward = 0
+    return next_state, reward
+
+def figure_3_2():
+    value = np.zeros((WORLD_SIZE, WORLD_SIZE))
+    while True:
+        new_value = np.zeros_like(value)
+        for i in range(WORLD_SIZE):
+            for j in range(WORLD_SIZE):
+                for action in ACTIONS:
+                    (next_i, next_j), reward = step([i, j], action)
+                    new_value[i, j] += ACTION_PROB * (reward + DISCOUNT * value[next_i, next_j])
+        if np.sum(np.abs(value - new_value)) < EPS:
+            return new_value
+        else:
+            value = new_value
+
+v = figure_3_2()
+for i in range(WORLD_SIZE):
+    for j in range(WORLD_SIZE):
+        sys.stdout.write("%5.2f " % v[i, j])
+    sys.stdout.write("\n")
