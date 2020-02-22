8d0551bf8de479993a91fb02e2abe9ad10adad5d
Sergey Litvinov <slitvinov@gmail.com>
2020-02-22 22:15:23 +0100

    minor

--- code/pole/example/ode.c
+++ code/pole/example/ode.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <pole.h>
 
+static const double STEP = 1e-6;
+
 static const char *me = "ode";
 static void
 usg(void)
 {
-    fprintf(stderr, "%s -m mc -f F -t t -d dt\n", me);
+    fprintf(stderr, "%s [-e end] [-m mc] [-f F] [-t t] [-d dt] [-x x]\n", me);
     exit(2);
 }
 
@@ -14,15 +16,26 @@ int
 main(int argc, char **argv)
 {
     enum {T = POLE_T, DT = POLE_DT, X = POLE_X, DX = POLE_DX};
-    double mc, F, t, dt, x;
+    double mc, F, t, dt, x, tend;
     struct PoleParam param;
     double y[POLE_N], dy[POLE_N];
+    struct ODE *ode;
+    double time;
+    int type;
     (void)argc;
     mc = F = t = dt = x = 0;
     while (*++argv != NULL && argv[0][0] == '-')
       switch (argv[0][1]) {
       case 'h':
 	usg();
+	break;
+      case 'e':
+	argv++;
+	if (*argv == NULL) {
+	  fprintf(stderr, "%s: -e needs an argument\n", me);
+	  exit(2);
+	}
+	tend = atof(*argv);
 	break;
       case 'm':
 	argv++;
@@ -69,24 +82,19 @@ main(int argc, char **argv)
 	exit(2);
       }
 
-    struct ODE *ode;
-    double dstep, tstep, time;
-    int type;
-    dstep = 1e-2;
     type = POLE_RK8PD;
     param.F = F;
     param.mc = mc;
-    tstep = 1.0;
     time = 0.0;
     y[T] = t;
     y[DT] = dt;
     y[X] = x;
     y[DX] = 0;
-    if (ode_ini(type, dstep, &param, &ode) != 0) {
+    if (ode_ini(type, STEP, &param, &ode) != 0) {
       fprintf(stderr, "%s: ode_ini failed\n", me);
       exit(2);
     };
-    if (ode_step(ode, tstep, &time, y) != 0) {
+    if (ode_step(ode, tend, &time, y) != 0) {
       fprintf(stderr, "%s: ode_step failed\n", me);
       exit(2);
     }
