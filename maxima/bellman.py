5813b182d7f80927ffc353f3b773ac61bcf1b868
Sergey Litvinov <slitvinov@gmail.com>
2020-03-06 18:10:52 +0100

    upd

--- maxima/bellman.py
+++ maxima/bellman.py
@@ -2,30 +2,35 @@
 
 import scipy.optimize
 import scipy
+import numpy
+import math
 
 X = 1
-M = 100
-N = 5
+M = 10
+N = 6
 
 a = 0.1
 b = 0.4
 
 def g(y):
-    return y
+    return math.sin(y)
 def h(y):
-    return y
+    return math.cos(y)
 
 def fun0(y, x):
     return g(y) + h(x - y)
 
-def fun(y, x, fn):
-    return g(y) + h(x - y) + fn(a*y + b*(x - y))
+def fn(y, t):
+    return numpy.interp(y, *t)[0]
+
+def fun(y, x, t):
+    return g(y) + h(x - y) + fn(a*y + b*(x - y), t)
 
 def maximize0(f, x, *args):
     y0 = x/2
     args = (x,) + args
-    res = scipy.optimize.minimize(f, x0, args = args, bounds = ((0, x),))
-    return res.x[0], res.fun[0]
+    res = scipy.optimize.minimize(f, y0, args = args, bounds = ((0, x),))
+    return res.x, res.fun
 
 def maximize(f, *args):
     xx = []
@@ -36,12 +41,12 @@ def maximize(f, *args):
         y = maximize0(f, x, *args)[1]
         xx.append(x)
         yy.append(y)
-    return lambda y: numpy.interp(y, xx, yy)
+    return xx, yy
 
-f = maximize(fun0)
-ff = [f]
+t = maximize(fun0)
+tt = [t]
 for i in range(N):
-    f = maximize(fun, f)
-    ff.append(f)
+    t = maximize(fun, t)
+    tt.append(t)
 
-ff.reverse()    
+tt.reverse()    
